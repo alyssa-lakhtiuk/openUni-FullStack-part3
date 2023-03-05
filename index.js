59700c90cb28cf38c1265a3274b14c29abe6cfa7
Аліса Лахтюк <alisalahtiuk@gmail.com>
2023-03-05 20:31:29 +0200

    3.4

--- index.js
+++ index.js
@@ -4,24 +4,24 @@ app.use(express.json())
 
 let persons = [
     { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+      id: 1,
+      name: "Arto Hellas", 
+      number: "040-123456"
     },
     { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+      id: 2,
+      name: "Ada Lovelace", 
+      number: "39-44-5323523"
     },
     { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+      id: 3,
+      name: "Dan Abramov", 
+      number: "12-43-234345"
     },
     { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+      id: 4,
+      name: "Mary Poppendieck", 
+      number: "39-23-6423122"
     }
 ]
 
@@ -50,7 +50,14 @@ app.get('/api/persons/:id', (request, response) => {
     }
   
     response.json(person)
-  })
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    console.log(`deleting person with id ${id}`)
+    response.status(204).end()
+})
 
 const PORT = 3001
 app.listen(PORT)
